2f545dfbd6e3fc240a146d4959741f905698cbed
Edgar Dewsbery <edewsbery@gmail.com>
2018-01-11 13:30:11 +0000

    start over

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-// an action creator is a function which returns an applications
-// an action is an object which provides details of the action we want to take. (Such as selecting a book, or in other words, setting the selected book)
-
-export function selectBook(book) {
-  console.log('A book has been selected: ', book.title);
-}
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import BooksReducer from './reducer_books';
-
-// this reducer combiner adds a key 'books' to our applications state. Where the key is 'books' and the value is whatevet gets returned from the BooksReducer reducer module
-const rootReducer = combineReducers({
-  books: BooksReducer
-});
-
-export default rootReducer;
